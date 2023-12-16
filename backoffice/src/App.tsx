f7e3c8cf6d79aa4f1c57074eeed613a686ee1c4a
Rabire <rabireh@outlook.fr>
2023-12-16 15:01:14 +0100

    chore: 📐 backoffice init

---
+++ backoffice/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <h1>HEllo world </h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
+
+export default App;
