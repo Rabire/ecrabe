e9eefae0ce97abd5faaea828de9607f16cf0a02a
oussama <oussamabechichi@gmail.com>
2024-07-02 11:48:23 +0200

    chore: Remove unused code and update component name in lesson layout

--- client/app/teacher/lesson/[id]/layout.tsx
+++ client/app/teacher/lesson/[id]/layout.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 import { SidebarNav } from "@/components/sidebar-nav";
 import { Separator } from "@/components/ui/separator";
 
@@ -22,30 +20,14 @@ const sidebarNavItems = [
   },
 ];
 
-interface SettingsLayoutProps {
+interface LessonLayoutProps {
   children: React.ReactNode;
 }
 
-export default function SettingsLayout({ children }: SettingsLayoutProps) {
+export default function SettingsLayout({ children }: LessonLayoutProps) {
   return (
     <>
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
+      <div className="space-y-6 p-10 pb-16">
         <div className="space-y-0.5">
           <h2 className="text-2xl font-bold tracking-tight">
             TODO:[Lesson Name]
