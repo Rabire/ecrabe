88bec3a3a3db0d92bb25c5686548bdbc9e0432cd
Rabire <rabireh@outlook.fr>
2024-07-29 16:50:46 +0200

    build errors

--- server/src/resolvers/types/comment.ts
+++ server/src/resolvers/types/comment.ts
@@ -4,10 +4,6 @@ const commentResolver: CommentResolvers = {
   author: ({ id }, _args, { prisma }) => {
     return prisma.comment.findUniqueOrThrow({ where: { id } }).author();
   },
-
-  chapter: ({ id }, _args, { prisma }) => {
-    return prisma.comment.findUnique({ where: { id } }).chapter();
-  },
 };
 
 export default commentResolver;
