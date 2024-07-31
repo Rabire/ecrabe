02930e331931a5ce5835af34e56c7b3afd873f74
Rabire <rabireh@outlook.fr>
2024-07-31 14:34:45 +0200

    rm loading card

--- client/components/loader.tsx
+++ client/components/loader.tsx
@@ -1,6 +1,5 @@
 import { cn } from "@/lib/utils";
 import { Loader2, LucideProps } from "lucide-react";
-import { Card, CardContent } from "./ui/card";
 
 export const LoadingWheel = ({ className, ...props }: LucideProps) => (
   <Loader2 className={cn("animate-spin", className)} {...props} />
@@ -21,46 +20,3 @@ export const LoadingText = ({
     {"x".repeat(charLength)}
   </span>
 );
-
-type Props = {
-  count?: number;
-  show?: boolean;
-  height?: number;
-  className?: string;
-};
-
-const MockCard = ({
-  height,
-  className,
-}: {
-  height?: number;
-  className?: string;
-}) => (
-  <Card
-    className={cn("pt-6", className)}
-    style={{ height: height ? `${height}px` : "auto" }}
-  >
-    <CardContent className="flex h-full flex-col justify-end gap-2">
-      <LoadingText className="h-4 w-2/3" />
-      <LoadingText className="h-4 w-1/3" />
-      <LoadingText className="h-4 w-3/4" />
-    </CardContent>
-  </Card>
-);
-
-export const LoadingCards = ({
-  count = 1,
-  show = true,
-  height,
-  className,
-}: Props) => {
-  if (!show) return null;
-
-  return (
-    <>
-      {Array.from({ length: count }, (_, i) => (
-        <MockCard key={i} height={height} className={className} />
-      ))}
-    </>
-  );
-};
